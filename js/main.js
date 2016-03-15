3458eb14da62ad35990d4b8d0eb8f3490d32812f
anthonyaubrey <anthonyaubrey245@gmail.com>
2016-03-15 13:52:20 -0400

    Added css and main js correections

--- js/main.js
+++ js/main.js
@@ -4,4 +4,16 @@ $ul.adClass('grains-list');
 
 grains.forEach(function (grain) {
   var $li = $('<li>');
-}
+  var $figure = $('<figure>');
+  var $img = $('<img>');
+  var $h2 = $('<h2>');
+  var $description = $('<p>');
+
+  $description.html(grain.desc);
+  $h2.html(grain.name);
+  $img.attr('src', 'images/' + grain.img);
+
+  $figure.append($img, $caption, $description);
+  $li.append($figure);
+  $li.append($li);
+});
